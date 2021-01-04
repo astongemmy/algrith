51d370db5af44511d0ce2c6b923c8c6856ffaf69
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-04 09:19:37 +0100

    Update Header.js

--- components/common/Header.js
+++ components/common/Header.js
@@ -7,7 +7,24 @@ export default class Header extends React.Component {
         super()
     }
 
+componentDidMount() {
+this.header = document.querySelector('#header')
+}
+
     render() {
+
+const handleScroll = () => {
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+        if (this.header !== undefined) {
+if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            this.header.querySelector('#brand-text').classList.remove('text-5xl');
+this.header.classList.querySelector('#brand-text').add('text-5xl');
+        } else {
+            this.header.querySelector('#brand-text').classList.remove('text-5xl');
+this.header.classList.querySelector('#brand-text').add('text-5xl');
+        }
+        }
+}
 
         return (
 
@@ -18,7 +35,7 @@ export default class Header extends React.Component {
                 </div> */}
                 <div className="ml-4 lg:ml-32 md:mt-4 mt-1">
                     <a href="/">
-                        <h1 className="uppercase hover:bg-green-400 p-4 rounded-full text-5xl text-yellow-100 mx-auto w-full text-center font-medium">
+                        <h1 id="brand-text" className="uppercase hover:bg-green-400 p-4 rounded-full text-5xl text-yellow-100 mx-auto w-full text-center font-medium">
                             Algrith
                         </h1>
                     </a>
