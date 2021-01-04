889c083b2961c74709251f824c5826ac18dc024f
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-04 08:04:05 +0100

    Update About.js

--- components/index/About.js
+++ components/index/About.js
@@ -17,10 +17,10 @@ export default class About extends React.Component {
                     Why choose us
                 </h1>
 
-                <div className="flex flex-wrap xl:flex-nowrap justify-start xl:justify-between items-center bg-grey-900 py-16 px-32 w-full dark:bg-black">
+                <div className="flex flex-wrap xl:flex-nowrap justify-start xl:justify-between items-center bg-grey-900 py-16 px-2 lg:px-32 w-full dark:bg-black">
 
                     <div className="w-full lg:w-2/4 xl:w-1/3 m-4 md:m-0 p-4">
-                        <div className="w-full p-16 bg-gray-100 text-gray-600 text-2xl rounded-xl text-center shadow">
+                        <div className="w-full p-8 lg:p-16 bg-gray-100 text-gray-600 text-2xl rounded-xl text-center shadow">
                             <h2 className="uppercase text-2xl text-black font-semibold mb-4">
                                 Competence
                             </h2>
@@ -32,7 +32,7 @@ export default class About extends React.Component {
                     </div>
 
                     <div className="w-full lg:w-2/4 xl:w-1/3 m-4 md:m-0 p-4">
-                        <div className="w-full p-16 bg-gray-100 text-gray-600 text-2xl rounded-xl text-center shadow">
+                        <div className="w-full p-8 lg:p-16 bg-gray-100 text-gray-600 text-2xl rounded-xl text-center shadow">
                             <h2 className="uppercase text-2xl text-black font-semibold mb-4">
                                 Fast
                             </h2>
@@ -44,7 +44,7 @@ export default class About extends React.Component {
                     </div>
 
                     <div className="w-full lg:w-2/4 xl:w-1/3 m-4 md:m-0 p-4">
-                        <div className="w-full p-16 bg-gray-100 text-gray-600 text-2xl rounded-xl text-center shadow">
+                        <div className="w-full p-8 lg:p-16 bg-gray-100 text-gray-600 text-2xl rounded-xl text-center shadow">
                             <h2 className="uppercase text-2xl text-black font-semibold mb-4">
                                 Trusted
                             </h2>
