e260bb1b4bb1620286ba1c6c47cd2e48290f4cbd
AstonGemmy <astongemmy@gmail.com>
2021-01-05 01:43:33 +0200

    Header changes

--- components/common/Header.js
+++ components/common/Header.js
@@ -5,41 +5,48 @@ export default class Header extends React.Component {
     
     constructor() {
         super()
-//this.handleScroll = this.handleScroll.bind(this)
+        this.handleScroll = this.handleScroll.bind(this)
     }
 
-componentDidMount() {
-this.header = document.querySelector('header')
-this.brandTitle = document.querySelector('#brand-title')
-//window​.​addEventListener​(​'scroll'​,​ ​this​.​handleScroll​,​ ​false​)​;
-​}​
+    componentDidMount() {
+        this.header = document.querySelector('header');
+        this.brand = document.querySelector('#brand');
+        this.brandTitle = document.querySelector('#brand-title');
+        this.hamburger = document.querySelector('#hamburger');
+        window.addEventListener('scroll', this.handleScroll, false)
+    }
 
-//componentWillUnmount​(​)​ ​{​
-    //window​.​removeEventListener​(​'scroll'​,​ ​this​.​handleScroll​,​ ​false​)​;​
-//}
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll, false)
+    }
+
+    handleScroll() {
+        // When the user scrolls down 200px from the top of the document, resize the navbar's padding and the logo's font size
+        if (this.header !== undefined) {
+            if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+                this.brandTitle.classList.remove('text-5xl')
+                this.brandTitle.classList.add('text-2xl')
+                // this.hamburger.classList.remove('p-4')
+                // this.hamburger.classList.add('p-2')
+                this.hamburger.classList.remove('text-3xl')
+                this.hamburger.classList.add('text-xl')
+                this.brand.classList.remove('md:mt-4')
+                this.brand.classList.add('md:mt-0')
+            } else {
+                this.brandTitle.classList.remove('text-2xl')
+                this.brandTitle.classList.add('text-5xl')
+                // this.hamburger.classList.remove('p-2')
+                // this.hamburger.classList.add('p-4')
+                this.hamburger.classList.remove('text-xl')
+                this.hamburger.classList.add('text-3xl')
+                this.brand.classList.remove('md:mt-0')
+                this.brand.classList.add('md:mt-4')
+            }
+        }
+    }
+
 
     render() {
-
-//const handleScroll = () => {
-// When the user scrolls down 200px from the top of the document, resize the navbar's padding and the logo's font size
-        //if (this.header !== undefined) {
-//if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-//this.brandTitle.classList.remove('text-5xl');
-//this.brandTitle.classList.add('text-2xl');
-//} else {
-           // this.brandTitle.classList.remove('text-2xl');
-//this.brandTitle.classList.add('text-5xl');
-        //}
-      //  }
-//}
-
-//useEffect(() => {
-    //window.addEventListener("scroll", handleScroll)
-    //return () => {
-      //window.removeEventListener("scroll", handleScroll)
-    //}
-  //})
-
 
         return (
 
@@ -48,7 +55,7 @@ this.brandTitle = document.querySelector('#brand-title')
                 {/* <div className="w-16 ml-4 lg:ml-64 md:mt-4 mt-1">
                     <img src="./images/logo/algrith.png" alt="brand-logo" />
                 </div> */}
-                <div className="ml-4 lg:ml-32 md:mt-4 mt-1">
+                <div id="brand" className="ml-4 lg:ml-32 md:mt-4 mt-0">
                     <a href="/">
                         <h1 id="brand-title" className="uppercase hover:bg-green-400 p-4 rounded-full text-5xl text-yellow-100 mx-auto w-full text-center font-medium">
                             Algrith
@@ -56,7 +63,7 @@ this.brandTitle = document.querySelector('#brand-title')
                     </a>
                 </div>
                 
-                <div id="hamburger" onClick={this.props.event.open_nav_bar} className="md:hidden flex justify-center items-center text-3xl cursor-pointer w-16 h-16 ml-4 mr-4 mt-1 bg-green-500 text-white">
+                <div id="hamburger" onClick={this.props.event.open_nav_bar} className="md:hidden flex justify-center items-center text-3xl cursor-pointer p-4 ml-4 mr-4 mt-1 bg-green-500 text-white">
                     <i className="fa fa-bars"></i>
                 </div>
 
