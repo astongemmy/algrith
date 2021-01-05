f8f3796509b888a91b5a2efea42f49c349a966e0
AstonGemmy <astongemmy@gmail.com>
2021-01-05 02:25:49 +0200

    Fresh changes

--- components/common/Header.js
+++ components/common/Header.js
@@ -24,15 +24,15 @@ export default class Header extends React.Component {
                 {/* <div className="w-16 ml-4 lg:ml-64 md:mt-4 mt-1">
                     <img src="./images/logo/algrith.png" alt="brand-logo" />
                 </div> */}
-                <div id="brand" className="ml-4 lg:ml-32 md:mt-4 mt-0">
+                <div id="brand" className="ml-4 lg:ml-32 md:mt-4 mt-0 transition-all ease-in-out duration-1000">
                     <a href="/">
-                        <h1 id="brand-title" className="uppercase hover:bg-green-400 p-4 rounded-full text-5xl text-yellow-100 mx-auto w-full text-center font-medium">
+                        <h1 id="brand-title" className="uppercase hover:bg-green-400 p-4 rounded-full text-5xl text-yellow-100 mx-auto w-full text-center font-medium transition-all ease-in-out duration-1000">
                             Algrith
                         </h1>
                     </a>
                 </div>
                 
-                <div id="hamburger" onClick={this.props.event.open_nav_bar} className="md:hidden flex justify-center items-center text-3xl cursor-pointer p-4 ml-4 mr-4 mt-1 bg-green-500 text-white">
+                <div id="hamburger" onClick={this.props.event.open_nav_bar} className="md:hidden flex justify-center items-center text-3xl cursor-pointer p-4 ml-4 mr-4 mt-1 bg-green-500 text-white transition-all ease-in-out duration-1000">
                     <i className="fa fa-bars"></i>
                 </div>
 
