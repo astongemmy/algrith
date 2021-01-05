de7b29e7305f0f5d3e295b78a56385cbe39bdf8a
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-05 10:19:34 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+backgroundImage: theme => ({
+         'intro': "url('/images/images (2).jpeg')",
+         //'footer-texture': "url('/img/footer-texture.png')",
+        })},
   },
   variants: {
     extend: {},
