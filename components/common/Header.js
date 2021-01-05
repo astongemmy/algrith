ce5bfe80fd190f6f11842f869177ea06caa2355c
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-05 10:27:38 +0100

    Update Header.js

--- components/common/Header.js
+++ components/common/Header.js
@@ -19,20 +19,20 @@ export default class Header extends React.Component {
 
         return (
 
-            <header className="flex sticky top-0 z-20 justify-between items-center bg-green-300 p-4 w-full dark:bg-black">
+            <header className="flex sticky top-0 z-20 justify-between items-center bg-white bg-geen-300 p-4 w-full dark:bg-black">
                 
                 {/* <div className="w-16 ml-4 lg:ml-64 md:mt-4 mt-1">
                     <img src="./images/logo/algrith.png" alt="brand-logo" />
                 </div> */}
                 <div id="brand" className="ml-4 lg:ml-24 transition-all ease-in-out duration-1000">
                     <a href="/">
-                        <h1 id="brand-title" className="flex items-center uppercase text-3xl md:text-5xl text-white mx-auto w-full text-center font-medium transition-all ease-in-out duration-1000">
+                        <h1 id="brand-title" className="flex items-center uppercase text-3xl md:text-5xl text-black mx-auto w-full text-center font-medium transition-all ease-in-out duration-1000">
                             Algrith
                         </h1>
                     </a>
                 </div>
                 
-                <div id="hamburger" onClick={this.props.event.open_nav_bar} className="md:hidden flex justify-center items-center text-3xl cursor-pointer p-4 ml-4 mr-1 text-white transform scale-y-75 transition-all ease-in-out duration-1000">
+                <div id="hamburger" onClick={this.props.event.open_nav_bar} className="md:hidden flex justify-center items-center text-3xl cursor-pointer p-4 ml-4 mr-1 text-black transform scale-y-75 transition-all ease-in-out duration-1000">
                     <i className="fa fa-bars"></i>
                 </div>
 
