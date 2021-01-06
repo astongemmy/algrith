7226fe8b49744e1594fea6f771eee1e52c825d3b
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-06 08:29:27 +0100

    Update Navbar.js

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -13,9 +13,9 @@ export default class Header extends React.Component {
 
             <nav id="nav-menu" className="navlinks transition-all z-50 ease-in-out duration-1000 shadow md:shadow-none bg-white flex md:flex fixed top-0 md:top-4 lg:top-16 -right-full md:right-12 flex-wrap md:flex-nowrap justify-start md:justify-end items-start md:items-center content-start w-screen md:w-auto h-screen md:h-12 text-gray-600 p-4 lg:bg-gradient-to-t lg:from-gray-100 lg:via-white lg:rounded-b-full">
                                 
-                <div className="md:hidden flex justify-between items-center m-1 text-left w-full text-3xl">
+                <div className="md:hidden flex justify-between items-center m-1 text-left w-full text-2xl">
                     
-                    <div className="flex text-black p-2">
+                    <div className="uppercase flex text-black p-2">
                         Algrith
                     </div>
 
