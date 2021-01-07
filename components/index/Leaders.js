b70b9a97b862b673d8b25237aae1328f94da15b4
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-07 09:21:46 +0100

    Update Leaders.js

--- components/index/Leaders.js
+++ components/index/Leaders.js
@@ -23,9 +23,9 @@ export default class About extends React.Component {
 
                 <div className="relative flex flex-wrap justify-around items-center px-2 xl:px-64 py-0 lg:py-16 w-full">
 
-                    <div className="relative bg-green-400 w-full lg:w-2/4 xl:w-2/4 my-0 mx-4 md:mx-0 p-0 md:p-4">
+                    <div className="relative w-full lg:w-2/4 xl:w-2/4 my-0 mx-4 md:mx-0 p-0 md:p-4">
 
-                        <div className="relative bg-yellow-200 w-full m-0 p-4 lg:p-12">
+                        <div className="relative w-full m-0 p-4 lg:p-12">
                         
                             <div className="z-10 uppercase absolute bg-green-500 text-green-100 top-10 left-10 p-4 rounded-xl">
                                 Secretary
@@ -35,7 +35,7 @@ export default class About extends React.Component {
 
                             <div className="absolute top-4 left-52 bg-red-400 transform rotate-45 w-4 h-32 border-t-4 border-r-4 border-red-600 rounded-xl"></div>
 
-                            <div className="flex relative mt-28 ml-20 w-52 h-52 rounded-full ring-8 ring-red-200 ring-offset-4 ring-offset-gray-100">
+                            <div className="flex relative mt-28 ml-20 w-52 h-52 rounded-full ring-8 ring-gray-200 ring-offset-4 ring-offset-gray-100">
                                 <div className="absolute w-full rounded-full h-full"></div>
                                 <div className="relative flex justify-center items-center w-full h-full rounded-full overflow-hidden">
                                     <img className="relative rounded-full" src="./images/board/secretary.jpg" width={200} height={200} alt="leader_image" />
@@ -50,9 +50,9 @@ export default class About extends React.Component {
 
                     </div>
                     
-                    <div className="relative bg-green-300 w-full lg:w-2/4 xl:w-2/4 my-0 mx-4 md:mx-0 p-0 md:p-4">
+                    <div className="relative w-full lg:w-2/4 xl:w-2/4 my-0 mx-4 md:mx-0 p-0 md:p-4">
 
-                        <div className="relative bg-yellow-200 w-full m-0 p-4 lg:p-12">
+                        <div className="relative w-full m-0 p-4 lg:p-12">
 
                             <div className="z-10 uppercase absolute bg-green-500 text-green-100 top-10 right-10 p-4 rounded-xl">
                                 Co Founder
@@ -60,9 +60,9 @@ export default class About extends React.Component {
                             
                             <div className="absolute top-16 right-16 w-20 h-32 border-t-4 border-l-4 border-green-400 rounded-xl"></div>
 
-                            <div className="absolute top-6 right-52 bg-red-400 transform -rotate-45 w-4 h-32 border-t-4 border-r-4 border-red-600 rounded-xl"></div>
+                            <div className="absolute top-4 right-52 bg-red-400 transform -rotate-45 w-4 h-32 border-t-4 border-r-4 border-red-600 rounded-xl"></div>
 
-                            <div className="flex relative mt-28 ml-auto mr-20 w-52 h-52 rounded-full ring-8 ring-red-200 ring-offset-4 ring-offset-gray-100">
+                            <div className="flex relative mt-28 ml-auto mr-20 w-52 h-52 rounded-full ring-8 ring-gray-200 ring-offset-4 ring-offset-gray-100">
                                 <div className="absolute w-full rounded-full h-full"></div>
                                 <div className="relative flex justify-center items-center w-full h-full rounded-full overflow-hidden">
                                     <img className="relative rounded-full" src="./images/board/cto.jpg" width={200} height={200} alt="leader_image" />
@@ -89,7 +89,7 @@ export default class About extends React.Component {
 
                             <div className="absolute top-32 left-2/4 transform -translate-x-2/4 bg-red-400 w-4 h-32 border-t-4 border-r-4 border-red-600 rounded-xl"></div>
 
-                            <div className="flex bg-gray-400 relative mt-48 mx-auto w-52 h-52 rounded-full ring-8 ring-red-200 ring-offset-4 ring-offset-gray-100">
+                            <div className="flex bg-gray-400 relative mt-48 mx-auto w-52 h-52 rounded-full ring-8 ring-gray-200 ring-offset-4 ring-offset-gray-100">
                                 <div className="absolute w-full rounded-full h-full"></div>
                                 <div className="relative flex justify-center items-center w-full h-full rounded-full overflow-hidden">
                                     <img className="relative rounded-full" src="./images/board/cto.jpg" width={200} height={200} alt="leader_image" />
