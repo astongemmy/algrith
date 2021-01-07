93eef7383dea088a1a619efe972f86c412b29528
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-07 14:49:50 +0100

    Update Contact.js

--- components/index/Contact.js
+++ components/index/Contact.js
@@ -27,23 +27,23 @@ export default class Contact extends React.Component {
 
                         <form method="post" className="relative flex flex-wrap w-full m-0 py-2 px-8 lg:p-12">
                             <div className="w-full lg:w-2/4 p-4">
-<label className="text-gray-600 p-2" for="contact-us-email">Email</label>
+<label className="text-gray-600 py-2" for="contact-us-email">Email</label>
                                 <input type="email" placeholder="Email" id="contact-us-email" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
                             </div>
                             <div className="w-full lg:w-2/4 p-4">
-<label className="text-gray-600 p-2" for="contact-us-name">Full Name</label>
+<label className="text-gray-600 py-2" for="contact-us-name">Full Name</label>
                                 <input type="text" placeholder="Name" id="contact-us-name" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
                             </div>
                             <div className="w-full lg:w-2/4 p-4">
-<label className="text-gray-600 p-2" for="contact-us-phone">Phone</label>
+<label className="text-gray-600 py-2" for="contact-us-phone">Phone</label>
                                 <input type="text" placeholder="Phone" id="contact-us-phone" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
                             </div>
                             <div className="w-full lg:w-2/4 p-4">
-<label className="text-gray-600 p-2" for="contact-us-location">Location</label>
+<label className="text-gray-600 py-2" for="contact-us-location">Location</label>
                                 <input type="text" placeholder="Location" id="contact-us-location" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
                             </div>
                             <div className="w-full p-4">
-<label className="text-gray-600 p-2" for="contact-us-message">Message</label>
+<label className="text-gray-600 py-2" for="contact-us-message">Message</label>
                                 <textarea placeholder="Write message..." id="contact-us-message" className="w-full outline-none h-32 ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
                             </div>
                             <button className="w-auto py-3 px-12 ml-4 my-8 text-2xl rounded-xl ring ring-offset-2 ring-green-500 bg-green-500 text-white">
