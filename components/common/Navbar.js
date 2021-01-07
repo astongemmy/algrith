a2d97bb47ca5c9da2cf52d6c794a68eb7480077f
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-07 22:18:08 +0100

    Update Navbar.js

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -11,7 +11,7 @@ export default class Header extends React.Component {
         
         return (
 
-            <nav id="nav-menu" className="navlinks transition-all z-50 ease-in-out duration-1000 shadow md:shadow-none bg-white flex fixed top-0 md:top-4 lg:top-16 -right-full md:right-12 flex-wrap md:flex-nowrap justify-start md:justify-end items-between w-screen md:w-auto h-screen md:h-12 text-gray-600 p-4 lg:bg-gradient-to-t lg:from-gray-100 lg:via-white lg:rounded-b-full">
+            <nav id="nav-menu" className="navlinks transition-all z-50 ease-in-out duration-1000 shadow md:shadow-none bg-white flex fixed top-0 md:top-4 lg:top-16 -right-full md:right-12 flex-wrap md:flex-nowrap justify-start md:justify-end items-between w-screen md:w-auto h-screen md:h-12 text-gray-600 p-0 lg:p-4 lg:bg-gradient-to-t lg:from-gray-100 lg:via-white lg:rounded-b-full">
                                 
                 <div className="md:hidden bg-red-300 flex justify-between items-center m-1 text-left w-full text-2xl">
                     
@@ -24,6 +24,7 @@ export default class Header extends React.Component {
                     </div>
 
                 </div>
+
 <div className="flex flex-wrap lg:flex-nowrap w-full bg-gray-200 p-8 rounded-xl mt-4 mx-auto">
                 <Link href="/about">
                     <a className="mt-1 mb-1 ml-8 mr-1 md:m-4 p-4 hover:bg-gray-200 text-left md:text-center w-full md:w-auto text-3xl md:text-xl text-green-500">About</a>
