acbb6a2872d264b72c86a54e628f38f5bec1aec7
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-07 13:29:17 +0100

    Update Contact.js

--- components/index/Contact.js
+++ components/index/Contact.js
@@ -14,7 +14,7 @@ export default class Contact extends React.Component {
             <div className="relative pt-16 pb-0 px-0 lg:px-32 w-full dark:bg-black">
 
 <div className="relative w-3/4 lg:w-auto mb-8 lg:mb-0 mx-auto lg:absolute top-auto lg:-top-56 rounded-full overflow-hidden p-0 lg:p-8 left-auto lg:-left-0 transform lg:scale-65 animation-all duration-1000 ease-in-out">
-                    <img className="rounded-full" src="./images/illustrations/contact-us.png" />
+                    <img className="rounded-full" src="./images/illustrations/contact-us.png" alt="contact us illustration image" />
                 </div>
 
                 <h1 className="relative uppercase text-3xl md:text-5xl text-red-600 mx-auto w-full text-center font-medium">
@@ -25,33 +25,38 @@ export default class Contact extends React.Component {
 
                     <div className="relative w-full md:w-2/4 m-0 p-4">
 
-                        <div className="relative flex flex-wrap w-full m-0 py-2 px-8 lg:p-12">
+                        <form method="post" className="relative flex flex-wrap w-full m-0 py-2 px-8 lg:p-12">
                             <div className="w-full lg:w-2/4 p-4">
-                                <input type="email" placeholder="Email" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
+<label for="contact-us-email"></label>
+                                <input type="email" placeholder="Email" id="contact-us-email" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
                             </div>
                             <div className="w-full lg:w-2/4 p-4">
-                                <input type="text" placeholder="Name" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
+<label for="contact-us-name"></label>
+                                <input type="text" placeholder="Name" id="contact-us-name" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
                             </div>
                             <div className="w-full lg:w-2/4 p-4">
-                                <input type="text" placeholder="Phone" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
+<label for="contact-us-phone"></label>
+                                <input type="text" placeholder="Phone" id="contact-us-phone" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
                             </div>
                             <div className="w-full lg:w-2/4 p-4">
-                                <input type="text" placeholder="Location" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
+<label for="contact-us-location"></label>
+                                <input type="text" placeholder="Location" id="contact-us-location" className="w-full outline-none ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
                             </div>
                             <div className="w-full p-4">
-                                <textarea placeholder="Write message..." className="w-full outline-none h-32 ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
+<label for="contact-us-message"></label>
+                                <textarea placeholder="Write message..." id="contact-us-message" className="w-full outline-none h-32 ring ring-gray-300 rounded p-4 my-4 mx-auto text-xl" />
                             </div>
                             <button className="w-auto py-3 px-12 ml-4 my-8 text-2xl rounded-xl ring ring-offset-2 ring-green-500 bg-green-500 text-green-100">
                                 Send
                             </button>
-                        </div>
+                        </form>
 
                     </div>
 
                     <div className="relative w-full md:w-2/4 m-0 p-0 md:p-4">
 
                         <div className="relative overflow-hidden flex justify-center lg:justify-start items-center w-full m-0 p-0 md:p-2 lg:p-12">
-                            <img src="./images/illustrations/man-on-laptop.png" />
+                            <img src="./images/illustrations/man-on-laptop.png" alt="contact us section image" />
                         </div>
 
                     </div>
