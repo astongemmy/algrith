4a997d355a0a4cb1c78156e676c72ecef073f38b
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-07 14:19:26 +0100

    Update Intro.js

--- components/index/Intro.js
+++ components/index/Intro.js
@@ -19,7 +19,7 @@ export default class Intro extends React.Component {
 
                 <div className="relative w-full bg-yellow-400 xl:w-1/3 m-0 md:m-4 px-8 py-2 lg:p-20">                    
                     <div className="relative bg-black">
-                        <button className="bg-green-500 relative rounded-full ring-4 ring-green-300 text-3xl text-white mx-4 mt-0 mb-8 md:my-4 px-8 lg:px-16 py-4 lg:py-8 focus:outline-none select-none focus:select-none">
+                        <button className="bg-green-500 relative rounded-full ring-4 ring-green-300 text-3xl text-white ml-0 mr-4 mt-0 mb-8 md:my-4 px-8 lg:px-16 py-4 lg:py-8 focus:outline-none select-none focus:select-none">
                             <div className="absolute top-0 -left-1 flex w-8 h-8">
                                 <div className="absolute inline-flex bg-yellow-500 w-full h-full animate-ping rounded-full"></div>
                                 <div className="relative inline-flex bg-green-400 w-8 h-8 rounded-full px-2 py-2"></div>
