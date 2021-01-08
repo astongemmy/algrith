9ce6afff090debec3366366a71cbfc21641702c5
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-08 05:50:53 +0100

    Update Navbar.js

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -13,7 +13,7 @@ export default class Header extends React.Component {
 
             <nav id="nav-menu" className="navlinks transition-all z-50 ease-in-out duration-1000 shadow md:shadow-none bg-white fixed top-0 md:top-4 lg:top-16 -right-full md:right-12 flex flex-col lg:flex-row justify-between md:justify-end lg:items-center content-betwen w-screen md:w-auto h-screen md:h-12 text-gray-600 p-0 lg:p-4 lg:bg-gradient-to-t lg:from-gray-100 lg:via-white lg:rounded-b-full">
                                 
-                <div className="md:hidden bg-red-300 flex flex-shrink justify-between content-center m-1 text-left w-full text-2xl">
+                <div className="md:hidden bg-red-300 flex flex-grow-0 flex-shrink justify-between content-center m-1 text-left w-full text-2xl">
                     
                     <h1 className="uppercase flex text-black p-2 font-medium">
                         Algrith
@@ -25,7 +25,7 @@ export default class Header extends React.Component {
 
                 </div>
 
-<div className="flex flex-col lg:flex-row flex-shrink-0 w-full bg-gray-200 p-8 rounded-xl mt-4 mx-auto">
+<div className="flex flex-col lg:flex-row flex-grow flex-shrink-0 w-full bg-gray-200 p-8 rounded-xl mt-4 mx-auto">
                 <Link href="/about">
                     <a className="mt-1 mb-1 ml-8 mr-1 md:m-4 p-4 hover:bg-gray-200 text-left md:text-center w-full md:w-auto text-3xl md:text-xl text-green-500">About</a>
                 </Link>
@@ -37,7 +37,7 @@ export default class Header extends React.Component {
                 </Link>
 </div>
 
-<div className="inline-flex flex-shrink relative p-4 w-full bg-gray-200">
+<div className="inline-flex flex-grow-0 flex-shrink relative p-4 w-full bg-gray-200">
 Powered by <span className="ml-1 text-green-600"> Algrith</span>
                     <img src="./images/logo/algrith.png" alt="brand logo" className="logo inline-flex" />
 </div>
