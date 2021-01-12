63c17e4ad512c2a492ff0e1e194d82a9f3e178af
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-12 14:19:56 +0100

    Update airbnb.js

--- pages/airbnb.js
+++ pages/airbnb.js
@@ -1,11 +1,27 @@
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://adminUser:@Kleezpass01@cluster0.zzdo1.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+const uri = "mongodb+srv://adminUser:%40Kleezpass01@cluster0.zzdo1.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const db = client.db("sample_airbnb");
 console.log("Connected to DB");
   // perform actions on the collection object
-  client.close();
+  //client.close();
+    findDocuments(db, function() {
+      client.close();
+    });
+
 });
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('listingsAndReviews');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
