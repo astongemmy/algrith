a8d429eb6b3285383432cb55f42feb13cc52430f
AstonGemmy <astongemmy@gmail.com>
2021-01-12 21:24:37 +0200

    Fresh

--- pages/about.js
+++ pages/about.js
@@ -1,25 +1,47 @@
 import React from 'react'
+import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../components/Layout'
+import Styles from '../components/styles/about/about.module.css'
+import Intro from '../components/index/Intro'
+import About from '../components/index/About'
 
 export default class About extends React.Component {
-
+  
     constructor() {
         super()
     }
 
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+        
+    }
+    
     render() {
         
         return (
-            <>
-                <h1>About Page</h1>
-                <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-                </h2>
-            </>
-        )
         
+            <Layout>
+                
+                <Head>
+                    <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1" />
+                    <title> About | Algrith </title>
+                </Head>
+
+                <main>
+
+                    <Intro />
+
+                    <About />             
+                
+                </main>
+
+            </Layout>
+    
+        )
+    
     }
 
 }
