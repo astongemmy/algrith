7c83010b8ccbb2bcb50fec8da6d93191a788e412
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-13 10:13:49 +0100

    Update airbnb.js

--- pages/api/airbnb.js
+++ pages/api/airbnb.js
@@ -8,7 +8,7 @@ const MongoClient = require('mongodb').MongoClient;
 }*/
 
 // Construct a document                                                                                                                                                              
-/*let contact = {
+let contact = {
     "name": { "first": "Alan", "last": "Turing" },
     "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
     "death": new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
@@ -16,24 +16,22 @@ const MongoClient = require('mongodb').MongoClient;
     "views": 1250000
 }
 
-const uri = `mongodb+srv://${PROCESS.ENV.MONGO_DB_USER}:${PROCESS.ENV.MONGO_DB_PASSWORD}cluster0.zzdo1.mongodb.net/${PROCESS.ENV.MONGO_DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.zzdo1.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 client.connect(async (req, res, err) => {
 
-    const collection = client.db(PROCESS.ENV.MONGO_DB_NAME).collection('contact');
+    const collection = client.db(process.env.MONGO_DB_NAME).collection('contact');
     
     // perform actions on the collection object  
 
     // Insert a single document, wait for promise so we can read it back
     const insert_contact = await collection.insertOne(contact);
     
-    const myDoc = await collection.findOne();*/
+    const myDoc = await collection.findOne();
     // Print to the console
-export default function(req, res) {
-    res.status(200).json(process.env.MONGO_DB_USER);
-}
+    res.status(200).json(myDoc);
 
     /*client.close();
 
-});*/
+});
