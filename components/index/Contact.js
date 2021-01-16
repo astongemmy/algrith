5138c985f2e006418e5e9bdfb734269947b4d4ef
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-16 16:58:45 +0100

    Update Contact.js

--- components/index/Contact.js
+++ components/index/Contact.js
@@ -173,7 +173,7 @@ Want to talk to us about anything?
 <li className="p-4">Feedback?</li>
 <li className="p-4">Questions?</li>
 </ul>
-<div className="text-blue-600 text-xl py-8 px-4 text-center bg-gradient-to-t from-red-400 via-transparent via-transparent rounded-b-xl">
+<div className="text-blue-600 text-xl py-8 px-4 text-center bg-gradient-to-t from-red-400 rounded-b-xl">
 We are one click away!
 </div>
 </div>
