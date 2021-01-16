e18725163d2ac62de0fc80da401058f66a49daf6
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-16 15:01:08 +0100

    Update Contact.js

--- components/index/Contact.js
+++ components/index/Contact.js
@@ -168,12 +168,12 @@ export default class Contact extends React.Component {
 <div className="text-3xl p-8 text-white rounded-t-xl bg-gradient-to-b from-red-400 via-red-400">
 Want to talk to us about anything?
 </div>
-<ul className="text-xl text-white text-shadow p-4">
+<ul className="text-xl text-blue-600 text-shadow p-4">
 <li className="p-4">Business?</li>
 <li className="p-4">Feedback?</li>
 <li className="p-4">Questions?</li>
 </ul>
-<div className="text-red-300 text-xl py-8 px-4 text-center">
+<div className="text-blue-600 text-xl py-8 px-4 text-center">
 We are one click away!
 </div>
 </div>
