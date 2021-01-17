fd0b52839e67a044fba71597c05740b472bda2bc
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-17 05:09:20 +0100

    Update Intro.js

--- components/about/Intro.js
+++ components/about/Intro.js
@@ -14,7 +14,7 @@ export default class Intro extends React.Component {
             <div className="relative w-full dark:bg-black bg-intro-about bg-left-top bg-cover bg-scroll lg:bg-fixed bg-no-repeat">
                 
                 <div className="w-full bg-black bg-opacity-30 m-0 md:m-4 p-8 lg:p-20">
-                    <h1 className="relative uppercase text-3xl md:text-7xl text-white p-8 m-0 md:m-4 w-full text-left font-bold">
+                    <h1 className="relative uppercase text-3xl md:text-7xl text-white py-8 m-0 md:m-4 w-full text-left font-bold">
                         Who we really are!
                     </h1>
                     <p className="w-full text-left text-white text-2xl lg:text-5xl">
@@ -25,7 +25,7 @@ export default class Intro extends React.Component {
                     </span>
                 </div>
 
-                <a href="#outline" className="scroll-selector cursor-pointer absolute flex justify-center items-center right-12 lg:right-32 -bottom-8 rounded-full bg-green-500 text-white p-8 text-3xl w-20 h-20"><i className="fa fa-arrow-down"></i></a>
+                <a href="#outline" className="scroll-selector cursor-pointer absolute flex justify-center items-center right-12 lg:right-32 -bottom-8 rounded-full bg-green-400 text-white p-8 text-3xl w-20 h-20"><i className="fa fa-arrow-down"></i></a>
 
             </div>
 
