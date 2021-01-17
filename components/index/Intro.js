7f8f39f99b73035be295267640540318cc3628b8
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-17 05:41:46 +0100

    Update Intro.js

--- components/index/Intro.js
+++ components/index/Intro.js
@@ -20,7 +20,7 @@ export default class Intro extends React.Component {
 
                 <div className="relative w-full xl:w-1/3 m-0 md:m-4 px-8 pb-16 lg:p-20">                    
                     <div className="relative">
-                        <a href="#outline" className="scroll-selector bg-green-400 relative rounded-full ring-4 ring-green-300 text-2xl font-bold uppercase text-white ml-2 mr-4 mt-0 mb-8 md:my-4 px-8 lg:px-16 py-4 lg:py-8 transform focus:scale-65 focus:select-none">
+                        <a href="#outline" className="scroll-selector bg-green-400 relative rounded-full ring-4 ring-green-300 text-2xl font-bold uppercase text-white ml-2 mr-4 mt-0 mb-8 md:my-4 px-8 lg:px-16 py-4 lg:py-8 transform hover:scale-65 select-none hover:select-none">
                             <div className="absolute top-0 -left-3 md:-left-1 flex w-8 h-8">
                                 <div className="absolute inline-flex bg-yellow-500 w-full h-full animate-ping rounded-full"></div>
                                 <div className="relative inline-flex bg-green-400 w-8 h-8 rounded-full px-2 py-2"></div>
