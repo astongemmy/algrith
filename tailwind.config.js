c60c0caa2a480be5c58f2d48b2b86b1bec64b39c
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-17 04:34:47 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: theme => ({
         'intro-index': "url('/images/sections/intro-index.jpeg')",
-'intro-about': "url('/images/sections/intro-about.jpg'), linear-gradient(to bottom, rgba(0,0,0,.6), rgba(255,255,255,.3))",
+'intro-about': "url('/images/sections/intro-about.jpg'), linear-gradient(to bottom, rgba(0,0,0,.9), rgba(255,255,255,.6))",
       }),
       scale: {
         '65': '.65',
