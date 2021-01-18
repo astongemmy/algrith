ccff990372727f1bb69fb300c8b39d916d536f16
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-18 10:00:54 +0100

    Update Intro.js

--- components/about/Intro.js
+++ components/about/Intro.js
@@ -14,15 +14,15 @@ export default class Intro extends React.Component {
             <div className="relative w-full dark:bg-black bg-intro-about bg-left-top bg-cover bg-scroll lg:bg-fixed bg-no-repeat">
                 
                 <div className="w-full bg-gradient-to-b from-white p-8 lg:p-20">
-                    <h1 className="relative uppercase text-3xl md:text-8xl text-green-500 py-8 m-0 w-full text-left font-bold">
+                    <h1 data-aos="fade-down" className="relative uppercase text-3xl md:text-8xl text-green-500 py-8 m-0 w-full text-left font-bold">
                         Who we really are!
                     </h1>
-                    <p className="w-full text-left text-gray-600 text-2xl lg:text-5xl">
+                    <p data-aos="fade-up" className="w-full text-left text-gray-600 text-2xl lg:text-5xl">
                         Algrith is a software company dedicated to delivering elegant, efficient, secure, accessible etc, web applications and websites  for use by clients both in the public and private sector.
                     </p>                    
-                    <span className="hidden mt-4 text-center text-black text-3xl font-medium lg:text-3xl">
+                    <aside data-aos="fade-right" className="mt-4 text-center text-navyblue text-3xl font-medium lg:text-3xl">
                         We make happy clients
-                    </span>
+                    </aside>
                 </div>
 
                 <span href="#outline" className="scroll-selector cursor-pointer absolute flex justify-center items-center right-12 lg:right-32 -bottom-8 rounded-full bg-green-400 text-white p-8 text-3xl w-20 h-20"><i className="fa fa-arrow-down"></i></span>
