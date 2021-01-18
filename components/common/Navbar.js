e14f393ea52e815745b69602ad2fafd5b9e460d4
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-18 10:08:34 +0100

    Update Navbar.js

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -15,38 +15,38 @@ export default class Header extends React.Component {
                                 
                 <div className="md:hidden flex flex-grow-0 flex-shrink justify-between content-center mb-2 text-left w-full text-2xl p-4">
                     
-                    <h1 data-aos="fade-right" className="uppercase flex text-black p-2 font-medium">
+                    <h1 id="navbar-title" className="uppercase flex text-black p-2 font-medium">
                         Algrith
                     </h1>
 
-                    <div data-aos="fade-left" id="close-menu" onClick={this.props.close_nav_bar} className="flex justify-center h-12 w-12 bg-gray-300 rounded-xl text-xl items-center text-black">
+                    <div id="close-menu" onClick={this.props.close_nav_bar} className="flex justify-center h-12 w-12 bg-gray-300 rounded-xl text-xl items-center text-black">
                         <i className="fa fa-times"></i>
                     </div>
 
                 </div>
                 
-                <div className="flex flex-col md:flex-row flex-grow flex-shrink-0 w-11/12 md:w-full p-2 rounded-xl my-4 lg:my-0 mx-auto">
+                <div id="nav-links" className="flex flex-col md:flex-row flex-grow flex-shrink-0 w-11/12 md:w-full p-2 rounded-xl my-4 lg:my-0 mx-auto">
                     
                     <Link href="/about">
-                        <a data-aos="fade-right" className="ripple-node transition all ease-in-out duration-700 my-2 mx-auto md:m-4 p-2 hover:border-opacity-100 border-green-400 border-b-4 border-opacity-0 text-left md:text-center w-full md:w-auto text-xl font-bold text-gray-600">About</a>
+                        <a className="ripple-node transition all ease-in-out duration-700 my-2 mx-auto md:m-4 p-2 hover:border-opacity-100 border-green-400 border-b-4 border-opacity-0 text-left md:text-center w-full md:w-auto text-xl font-bold text-gray-600">About</a>
                     </Link>
                     <Link href="/contact">
-                        <a data-aos="fade-right" className="ripple-node transition all ease-in-out duration-700 my-2 mx-auto md:m-4 p-2 hover:border-opacity-100 border-green-400 border-b-4 border-opacity-0 text-left md:text-center w-full md:w-auto text-xl font-bold text-gray-600">Contact</a>
+                        <a className="ripple-node transition all ease-in-out duration-700 my-2 mx-auto md:m-4 p-2 hover:border-opacity-100 border-green-400 border-b-4 border-opacity-0 text-left md:text-center w-full md:w-auto text-xl font-bold text-gray-600">Contact</a>
                     </Link>
                     <Link href="/support">
-                        <a data-aos="fade-right" className="ripple-node transition all ease-in-out duration-700 my-2 mx-auto md:m-4 p-2 hover:border-opacity-100 border-green-400 border-b-4 border-opacity-0 text-left md:text-center w-full md:w-auto text-xl font-bold text-gray-600">Support</a>
+                        <a className="ripple-node transition all ease-in-out duration-700 my-2 mx-auto md:m-4 p-2 hover:border-opacity-100 border-green-400 border-b-4 border-opacity-0 text-left md:text-center w-full md:w-auto text-xl font-bold text-gray-600">Support</a>
                     </Link>
 
                 </div>
 
                 <div className="md:hidden flex justify-between items-center flex-grow-0 flex-shrink relative p-4 w-full bg-gray-200 mt-2">
                     
-                    <div data-aos="fade" className="inline-flex items-center h-full">
+                    <div id="footer-brand" className="inline-flex items-center h-full">
                         Powered by <span className="mx-1 text-navyblue"> Algrith</span>
                         <img src="./images/logo/algrith.png" alt="brand logo" className="logo inline-flex" />
                     </div>
                     
-                    <div data-aos="fade-left" className="flex justify-center items-center h-full">
+                    <div id="footer-socials" className="flex justify-center items-center h-full">
                         <span className="flex justify-center mx-1 items-center rounded-xl text-gray-600 text-xl h-8 w-8 bg-gray-300">
                             <i className="fab fa-facebook"></i>
                         </span>
