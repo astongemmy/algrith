7c4538cce3a0d42039e872cfc95f3ac07155d9bf
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-18 10:18:20 +0100

    Update Navbar.js

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -25,7 +25,7 @@ export default class Header extends React.Component {
 
                 </div>
                 
-                <div id="nav-links" className="flex flex-col md:flex-row flex-grow flex-shrink-0 w-11/12 md:w-full p-2 rounded-xl my-4 lg:my-0 mx-auto">
+                <div id="navbar-links" className="flex flex-col md:flex-row flex-grow flex-shrink-0 w-11/12 md:w-full p-2 rounded-xl my-4 lg:my-0 mx-auto">
                     
                     <Link href="/about">
                         <a className="ripple-node transition all ease-in-out duration-700 my-2 mx-auto md:m-4 p-2 hover:border-opacity-100 border-green-400 border-b-4 border-opacity-0 text-left md:text-center w-full md:w-auto text-xl font-bold text-gray-600">About</a>
@@ -41,12 +41,12 @@ export default class Header extends React.Component {
 
                 <div className="md:hidden flex justify-between items-center flex-grow-0 flex-shrink relative p-4 w-full bg-gray-200 mt-2">
                     
-                    <div id="footer-brand" className="inline-flex items-center h-full">
+                    <div id="navbar-footer-brand" className="inline-flex items-center h-full">
                         Powered by <span className="mx-1 text-navyblue"> Algrith</span>
                         <img src="./images/logo/algrith.png" alt="brand logo" className="logo inline-flex" />
                     </div>
                     
-                    <div id="footer-socials" className="flex justify-center items-center h-full">
+                    <div id="navbar-footer-socials" className="flex justify-center items-center h-full">
                         <span className="flex justify-center mx-1 items-center rounded-xl text-gray-600 text-xl h-8 w-8 bg-gray-300">
                             <i className="fab fa-facebook"></i>
                         </span>
