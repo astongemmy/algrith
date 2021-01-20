516b286ea32958a8375f396c100a93229c7ea87a
AstonGemmy <astongemmy@gmail.com>
2021-01-20 16:08:37 +0100

    Updated AOS plugin

--- components/Layout.js
+++ components/Layout.js
@@ -19,9 +19,6 @@ export default class Layout extends React.Component {
     }
 
     componentDidMount() {
-        // AOS.init({
-        //     easing: 'ease-in-out-sine'
-        // });          
         // Selector used in multiple methods
         this.nav_menu = document.querySelector('#nav-menu')
         this.overlay = document.querySelector('#overlay')
