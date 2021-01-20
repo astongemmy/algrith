a90c01f61016cfad8e38b1d8a2c40867c4436168
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-20 20:34:38 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import '../public/styles/App.css'
 import '../public/styles/Icons.css'
 import '../public/styles/aos.css'
 import '../public/styles/animate.css'
-import '../public/js/aos.js'
-import '../public/js/aos-init.js'
+/*import '../public/js/aos.js'
+import '../public/js/aos-init.js'*/
 import App from 'next/app'
 
 export default class MyApp extends App {
