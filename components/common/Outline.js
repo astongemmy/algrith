863b17dc83b578cef2e82655b714a69e10f66b6f
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-21 09:01:44 +0100

    Update Outline.js

--- components/common/Outline.js
+++ components/common/Outline.js
@@ -15,7 +15,7 @@ export default class Outline extends React.Component {
             return (
                 <div key={outline.title} data-aos="fade-up" className="w-full lg:w-2/4 xl:w-1/3 m-4 md:m-0 p-4">
                     <div className="w-full p-8 lg:p-16 bg-gray-100 text-gray-600 text-lg md:text-2xl rounded-xl text-center shadow">
-                        <h2 className="uppercase text-xl md:text-2xl text-black font-semibold mb-4">
+                        <h2 className="uppercase text-xl md:text-2xl text-gray-600 font-semibold mb-4">
                             {outline.title}
                         </h2>
                         <span className="flex justify-center items-center my-2 mx-auto p-1 text-5xl text-green-300 bg-green-100 w-20 h-20 rounded-full">
