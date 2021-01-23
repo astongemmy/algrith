fff8d673b446d862c3727ad86ecdfc112b64735c
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-23 19:13:21 +0100

    Update Footer.js

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -10,14 +10,14 @@ export default class Footer extends React.Component {
 
         return (
 
-            <footer className="bg-gray-200">
+            <footer className="bg-gray-100">
 
-<div className="w-full flex justify-between items-start flex-wrap">
-<a className="w-2/4 lg:text-center p-4 text-xl text-green-600">Home</a>
-<a className="w-2/4 lg:text-center p-4 text-xl text-green-600">About</a>
-<a className="w-2/4 lg:text-center p-4 text-xl text-green-600">Contact Us</a>
-<a className="w-2/4 lg:text-center p-4 text-xl text-green-600">Support Us</a>
-<a className="w-2/4 lg:text-center p-4 text-xl text-green-600">FAQ</a>
+<div className="w-full p-8 flex justify-between items-start flex-wrap">
+<a className="w-2/4 lg:text-center p-4 text-xl text-gray-600">Home</a>
+<a className="w-2/4 lg:text-center p-4 text-xl text-gray-600">About</a>
+<a className="w-2/4 lg:text-center p-4 text-xl text-gray-600">Contact Us</a>
+<a className="w-2/4 lg:text-center p-4 text-xl text-gray-600">Support Us</a>
+<a className="w-2/4 lg:text-center p-4 text-xl text-gray-600">FAQ</a>
 </div>
 
                 <a href="/" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center m-6 text-xl">
