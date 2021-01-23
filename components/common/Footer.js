b38348fa4bdc69ef73a5171da20af16ad28a180e
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-23 18:52:05 +0100

    Update Footer.js

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -12,6 +12,14 @@ export default class Footer extends React.Component {
 
             <footer className="p-4">
 
+<div className="flex justify-center align-start flex-wrap bg-red-400">
+<a className="p-4 text-xl text-gray-600">Home</a>
+<a className="p-4 text-xl text-gray-600">About</a>
+<a className="p-4 text-xl text-gray-600">Contact Us</a>
+<a className="p-4 text-xl text-gray-600">Support Us</a>
+<a className="p-4 text-xl text-gray-600">FAQ</a>
+</div>
+
                 <a href="/" target="_blank" rel="noopener noreferrer" className="text-xl">
 
                     Powered by <span className="ml-1 text-navyblue"> Algrith</span>
