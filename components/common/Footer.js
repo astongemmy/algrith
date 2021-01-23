6c2387967bde074c655d28c55fc1e81c0aa149eb
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2021-01-23 19:00:21 +0100

    Update Footer.js

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -12,15 +12,15 @@ export default class Footer extends React.Component {
 
             <footer className="p-4">
 
-<div className="w-full flex justify-center align-start flex-wrap bg-red-400">
-<a className="w-2/4 p-4 text-xl text-gray-600">Home</a>
-<a className="w-2/4 p-4 text-xl text-gray-600">About</a>
-<a className="w-2/4 p-4 text-xl text-gray-600">Contact Us</a>
-<a className="w-2/4 p-4 text-xl text-gray-600">Support Us</a>
-<a className="w-2/4 p-4 text-xl text-gray-600">FAQ</a>
+<div className="w-full flex justify-between items-start flex-wrap bg-red-400">
+<a className="w-2/4 text-center p-4 text-xl text-gray-600">Home</a>
+<a className="w-2/4 text-center p-4 text-xl text-gray-600">About</a>
+<a className="w-2/4 text-center p-4 text-xl text-gray-600">Contact Us</a>
+<a className="w-2/4 text-center p-4 text-xl text-gray-600">Support Us</a>
+<a className="w-2/4 text-center p-4 text-xl text-gray-600">FAQ</a>
 </div>
 
-                <a href="/" target="_blank" rel="noopener noreferrer" className="text-xl">
+                <a href="/" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center text-xl">
 
                     Powered by <span className="ml-1 text-navyblue"> Algrith</span>
                     <img src="./images/logo/algrith.png" alt="Algrith Logo" className="logo" />
