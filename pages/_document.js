65a8a75c35f598675bf15b0e6700fc42a41e6b57
Uwakmfon Akpan <astongemmy@gmail.com>
2022-05-24 02:57:51 +0100

    Favicon fixes and general cleansing

--- pages/_document.js
+++ pages/_document.js
@@ -2,28 +2,23 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        
-        return { ...initialProps }
-    
+        const initialProps = await Document.getInitialProps(ctx)        
+        return { ...initialProps }    
     }
     
-    render() {
-        
-        return (
-        
-            <Html lang="en-US">
-                
+    render() {        
+        return (        
+            <Html lang="en-US">                
                 <Head>
                     <meta name="theme-color" content="" />
                     <meta name="msapplication-TileColor" content="#ffffff" />
-                    <meta name="msapplication-TileImage" content="images/favicon/ms-icon-144x144.png" />
+                    {/* <meta name="msapplication-TileImage" content="images/favicon/ms-icon-144x144.png" /> */}
                     
                     <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
                     <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-                    <meta name="description" content="Algrith Official Website" />
-                    <meta property="og:description" name="description" content="Algrith Official Website" />
-                    <meta property="og:author" name="author" content="Aston Gemmy, Ilori Jide" />
+                    <meta name="description" content="Algrith LLC" />
+                    <meta property="og:description" name="description" content="Algrith LLC Website" />
+                    <meta property="og:author" name="author" content="Algrith LLC" />
                     <meta property="og:keywords" name="keywords" content="Business, Artisans, Buyers, Sellers, Online Market, Sales" />
                     <meta property="og:image" name="image" content="/images/logo/algrith.png" />
                     <link rel="manifest" href="/manifest.json" />
@@ -31,35 +26,21 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
-                    <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png" />
-                    <link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png" />
-                    <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-icon-72x72.png" />
-                    <link rel="apple-touch-icon" sizes="76x76" href="images/favicon/apple-icon-76x76.png" />
-                    <link rel="apple-touch-icon" sizes="114x114" href="images/favicon/apple-icon-114x114.png" />
-                    <link rel="apple-touch-icon" sizes="120x120" href="images/favicon/apple-icon-120x120.png" />
-                    <link rel="apple-touch-icon" sizes="144x144" href="images/favicon/apple-icon-144x144.png" />
-                    <link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-icon-152x152.png" />
-                    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-icon-180x180.png" />
-                    <link rel="icon" type="image/png" sizes="192x192" href="images/favicon/android-icon-192x192.png" />
+                    <link rel="icon" type="image/png" sizes="512x512" href="images/favicon/android-chrome-512x512.png" />
+                    <link rel="icon" type="image/png" sizes="192x192" href="images/favicon/android-chrome-192x192.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png" />
-                    <link rel="icon" type="image/png" sizes="96x96" href="images/favicon/favicon-96x96.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png" />
-                        
-                    <link rel="shortcut icon" href="images/favicon/algrith.png" />
+                    <link rel="icon" type="image/png" href="images/favicon/apple-touch-icon.png" />
+                    <link rel="shortcut icon" type="image/png" href="images/favicon/favicon.ico" />
+                    
                     <script type="text/javascript" src="/js/aos.js"></script>
                 </Head>
                 
-                <body>
-                    
+                <body>                    
                     <Main />
-                    <NextScript />
-                    
+                    <NextScript />                    
                 </body>
-
             </Html>
-            
-        )
-    
+        )    
     }
-
 }
