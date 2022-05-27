1303542d991e74bcb2a75866fb96a9ab06a3b8b2
Uwakmfon Akpan <astongemmy@gmail.com>
2022-05-27 22:03:09 +0100

    Added responsive thumbnail positioning to react-image-gallery component

--- components/LeftNav.js
+++ components/LeftNav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const LeftNav = (onClick, disabled) => {
   return (
-    <a onClick={onClick} disabled={disabled} className="text-xl absolute left-8 top-2/4 -translate-y-2/4 z-10 flex justify-center items-center h-14 w-14 text-white bg-zinc-500 rounded-full focus:bg-zinc-800 hover:bg-zinc-800" href="#">
+    <a onClick={onClick} disabled={disabled} className="text-xl absolute left-2 top-2/4 -translate-y-2/4 z-10 flex justify-center items-center h-14 w-14 text-white bg-zinc-500 rounded-full focus:bg-zinc-800 hover:bg-zinc-800" href="#">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
         <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
       </svg>
--- components/ThumbInner.js
+++ components/ThumbInner.js
@@ -1,7 +1,7 @@
 import React from "react";
 const ThumbInner = (item) => {
   return (
-    <a className="overflow-hidden rounded-lg flex items-center justify-center w-full h-32" href="#">
+    <a className="overflow-hidden rounded-lg flex items-center justify-center w-full h-24 md:h-32" href="#">
       <img className="rounded-lg object-cover object-center w-full h-full" src={item.thumbnail} alt="" />
     </a>
   )
