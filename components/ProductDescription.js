6e544d4f38497353dd0134411503026a7cf904c5
Uwakmfon Akpan <astongemmy@gmail.com>
2022-05-30 21:06:06 +0100

    Fixed bug that prevented deployment

--- components/ProductDescription.js
+++ components/ProductDescription.js
@@ -5,7 +5,7 @@ export default function ProductDescription({ product }) {
   return (
     <div className="hidden lg:block my-8 xl:mt-0 xl:w-2/5 pl-2 xl:pl-8">
       <h2 className="capitalize mb-2 text-3xl md:text-4xl font-heading font-medium">
-        { product.type } Products
+        { product.name } Products
       </h2>
       <span className="inline-block mb-4">
         <ProductRating />
