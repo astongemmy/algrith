7bbe81faa303826d8ab2d354c00f01ce8debced1
Uwakmfon Akpan <astongemmy@gmail.com>
2022-06-07 13:53:01 +0100

    Added login and sign up pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -46,7 +46,7 @@ export default function Navbar({ closeMenuRef, navbarRef }) {
 					</a>
 				</Link>
 				<Link href="/login">
-					<a className="flex items-center justify-between bg-green-500 rounded ripple-node transition all ease-in-out duration-400 my-2 mx-auto md:mx-4 py-3 px-4 md:py-2 md:px-4 hover:border-opacity-100 border-green-400 border-b-4 border-opacity-0 text-left md:text-center w-full md:w-auto text-xl font-bold text-white">
+					<a className="flex items-center justify-between bg-green-500 rounded md:rounded-full ripple-node transition all ease-in-out duration-400 my-2 mx-auto md:mx-4 py-3 px-4 md:py-2 md:px-4 hover:border-opacity-100 border-green-400 border-b-4 border-opacity-0 text-left md:text-center w-full md:w-auto text-xl font-bold text-white">
 						Login
 						<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
 							<path strokeLinecap="round" strokeLinejoin="round" d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
