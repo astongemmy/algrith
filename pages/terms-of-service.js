88e4a58d6f2066d34ba9a0698f3b0d5a50ba5937
Uwakmfon Akpan <astongemmy@gmail.com>
2022-06-08 17:34:35 +0100

    Updated How it works page

--- pages/terms-of-service.js
+++ pages/terms-of-service.js
@@ -27,7 +27,7 @@ export default function TermsOfService() {
             </div>
             <div className="flex flex-wrap w-full">
               <div className="bg-white shadow-sm xl:w-9/12 text-xl p-4 md:p-10 text-justify">
-                <div className="relative pb-16">
+                <div className="relative pb-4 md:pb-16">
                   <p className="mb-12">
                     These Terms and Conditions ("Terms", "Terms and Conditions") govern your
                     relationship with/activities on https://algrith.com (the "Service") website operated by
