296a2b108d32d5a01e90b69cb20711d024f3cce4
Uwakmfon Akpan <astongemmy@gmail.com>
2022-06-09 07:16:45 +0100

    Removed Home link from Header component

--- components/Footer.js
+++ components/Footer.js
@@ -5,9 +5,9 @@ export default function Footer() {
 	return (
 		<footer className="relative bg-gray-100">
 			<div className="flex flex-wrap justify-between px-4 md:px-8 lg:px-16 xl:px-24 pb-12">
-				<div className="w-full md:w-2/5 lg:w-3/12 pt-4">
+				<div className="w-full md:w-2/5 lg:w-3/12 pt-8">
 					<h1 className="p-4 text-navyblue text-xl font-bold">
-						<svg xmlns="http://www.w3.org/2000/svg" className="hidden md:inline mr-4 text-green-300 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+						<svg xmlns="http://www.w3.org/2000/svg" className="hidden md:inline mr-4 text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
 							<path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
 						</svg>
 						Featured
@@ -54,7 +54,7 @@ export default function Footer() {
 				<div className="flex flex-wrap justify-between w-full md:w-3/5 lg:w-9/12 pt-4 md:pl-4">
 					<div className="w-1/2 lg:w-1/4 pt-4">
 						<h1 className="p-4 text-navyblue font-bold text-xl">
-							<svg xmlns="http://www.w3.org/2000/svg" className="hidden md:inline mr-4 text-green-300 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+							<svg xmlns="http://www.w3.org/2000/svg" className="hidden md:inline mr-4 text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
 								<path strokeLinecap="round" strokeLinejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
 							</svg>
 							Company
@@ -91,8 +91,8 @@ export default function Footer() {
 					</div>
 					<div className="w-1/2 lg:w-1/4 pt-4">
 						<h1 className="p-4 text-navyblue font-bold text-xl">
-							<svg xmlns="http://www.w3.org/2000/svg" className="hidden md:inline mr-4 text-green-300 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-								<path strokeLinecap="round" strokeLinejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+							<svg xmlns="http://www.w3.org/2000/svg" className="hidden md:inline mr-4 text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+								<path strokeLinecap="round" strokeLinejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
 							</svg>
 							Products
 						</h1>
@@ -113,7 +113,7 @@ export default function Footer() {
 					</div>
 					<div className="w-1/2 lg:w-1/4 pt-4">
 						<h1 className="p-4 text-navyblue font-bold text-xl">						
-							<svg xmlns="http://www.w3.org/2000/svg" className="hidden md:inline mr-4 text-green-300 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+							<svg xmlns="http://www.w3.org/2000/svg" className="hidden md:inline mr-4 text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
 								<path strokeLinecap="round" strokeLinejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
 							</svg>
 							Legal
@@ -135,7 +135,7 @@ export default function Footer() {
 					</div>
 					<div className="w-1/2 lg:w-1/4 pt-4">
 						<h1 className="p-4 font-bold text-xl">
-							<svg xmlns="http://www.w3.org/2000/svg" className="hidden md:inline mr-4 text-green-300 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+							<svg xmlns="http://www.w3.org/2000/svg" className="hidden md:inline mr-4 text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
 								<path strokeLinecap="round" strokeLinejoin="round" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
 							</svg> Contact Us
 						</h1>
