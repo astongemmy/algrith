c1b3f502c67e84b44fa6fbc2a9061ba04b10913b
Uwakmfon Akpan <astongemmy@gmail.com>
2022-07-29 19:09:45 +0100

    Modified order successfull page

--- pages/order-successful.js
+++ pages/order-successful.js
@@ -13,10 +13,10 @@ export default function OrderSuccessful() {
       <main>
         <section className="h-screen w-full 2xl:w-9/12 2xl:mx-auto p-8">
           <div className="h-full flex flex-col justify-center items-center mx-auto w-full md:w-6/12 lg:w-5/12 xl:w-3/12">
-            <div className="shadow rounded-2xl dark:bg-slate-800 bg-white w-full pt-10 pb-10 px-6">
+            <div className="shadow rounded-2xl dark:bg-slate-800 bg-white w-full py-12 px-6">
               
               <div className="pt-5 text-center">
-                <div className="flex flex-col justify-between items-center gap-y-10">
+                <div className="flex flex-col justify-between items-center gap-y-12">
                   <span className="text-green-500 rounded-full border-2 border-green-500">
                     <svg className="" width="64" height="64" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M6.81671 15.0418L0 8.2251L0.90027 7.32483L6.81671 13.2413L19.0997 0.958252L20 1.85852L6.81671 15.0418Z" fill="currentColor"></path>
@@ -28,7 +28,7 @@ export default function OrderSuccessful() {
                 </div>
 
                 <Link href={'/'}>
-                  <a className="text-md font-medium text-heading px-2 py-1 mt-5 block text-white dark:bg-opacity-50 bg-green-500 shadow-sm rounded-full">
+                  <a className="text-md font-medium text-heading px-2 py-3 mt-12 block text-white dark:bg-opacity-50 bg-green-500 shadow-sm rounded-full">
                     Back to homepage
                   </a>
                 </Link>
